9da41be273b05998869ca3c08c235f80bb508903
Kwazinator <kwasiky@gmail.com>
2020-03-09 21:01:32 -0400

    sdf

--- flaskr/services/GraphService.py
+++ flaskr/services/GraphService.py
@@ -5,7 +5,7 @@ from dateutil.relativedelta import relativedelta
 from flask import current_app
 import re
 
-class MenuItemService:
+class GraphService:
 
     def __init__(self):
         pass
@@ -13,13 +13,13 @@ class MenuItemService:
     def get_menu_item_by_names(self, name):
         #maybe do some regex
         name = name.lower() #make lowercase
-        return MenuItemDAO().get_menu_item_by_names(name)
+        return GraphDAO().get_menu_item_by_names(name)
 
     def get_menu_item_by_id(self, id):
-        return MenuItemDAO().get_menu_item_by_id(id)
+        return GraphDAO().get_menu_item_by_id(id)
 
     def insert_menu_item(self, name, price, restaurant):
-        result = MenuItemDAO().insert_menu_item(name,price,restaurant)
+        result = GraphDAO().insert_menu_item(name,price,restaurant)
         if result == None:
             return 'Unable to add item to website'
         else:
